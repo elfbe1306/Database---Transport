c5d4932cf2d5340a677ec917cee34f19be55b876
elfbe <huynhngocnhon2004@gmail.com>
2024-12-09 01:41:54 +0700

    testing crud

--- src/Pages/DashBoard.jsx
+++ src/Pages/DashBoard.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import supabase from '../supabase-client'
 
 export const DashBoard = () => {
+  useEffect(() => {
+    fetchWarehouse();
+    functionGetWarehouse();
+  }, [])
 
-  
+  const fetchWarehouse = async () => {
+    const {data, error} = await supabase.from("warehouse").select("*");
+
+    if(error) {
+      console.log("Error fetching", error);
+    } else {
+      console.log(data);
+    }
+  }
+
+  const functionGetWarehouse = async () => {
+    const {data, error} = await supabase.rpc('Get_all_data_from_warehouse');
+
+    if(error) {
+      console.log("Error fetching", error);
+    } else {
+      console.log(data);
+    }
+  }
+
   return (
     <div>DashBoard</div>
   )
