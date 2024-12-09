622265c313d2888a2c9cbfeb729eeab074dc67f2
elfbe <huynhngocnhon2004@gmail.com>
2024-12-09 12:36:38 +0700

    sắp xong dashboard

--- src/Pages/DashBoard.jsx
+++ src/Pages/DashBoard.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 // import supabase from '../supabase-client';
 import { Header } from '../components/Header';
 import { InfoBlock } from '../components/DashBoard_Components/InfoBlock';
 import styles from '../Styles/DashBoard_Styles/DashBoard.module.css';
+import { TableComponent } from '../components/DashBoard_Components/TableComponent';
 
 export const DashBoard = () => {
   const productData = [
@@ -16,6 +17,28 @@ export const DashBoard = () => {
     { label: 'CANCEL', value: 3, color: '#DC4446' },
   ];
 
+  const rowData1 = [
+    { store: "Nguyen Van Troi", product: "Candid Retinol Essence 0.5", total: "1", choose: "View branch", top: 100 },
+    { store: "Nguyen Van Troi", product: "Candid Retinol Essence 1", total: "2", choose: null, top: 142 },
+    { store: "To Hien Thanh", product: "Candid AHA & PHA Exfoliating Gel", total: "4", choose: null, top: 188 },
+  ];
+  const rowData2 = [
+    { store: "Nguyen Van Troi", quantity: "5 cartons", duration: "Tomorrow", top: 100 },
+    { store: "Nguyen Trai", quantity: "6 cartons", duration: "Today", top: 142 },
+    { store: "To Hien Thanh", quantity: "3 cartons", duration: "4 days next", top: 188 },
+  ];
+
+  const [showAssignContainer, setShowAssignContainer] = useState(false);
+
+  const toggleAssignContainer = () => {
+    setShowAssignContainer(!showAssignContainer);
+  };
+  if(showAssignContainer){
+    document.body.classList.add(styles.active_modal)
+  }else{
+    document.body.classList.remove(styles.active_modal)
+  }
+
   return (
     <div>
       <Header></Header>
@@ -24,6 +47,36 @@ export const DashBoard = () => {
         <InfoBlock title="PRODUCTS" data={productData} headerColor="#C5D2E4" />
         <InfoBlock title="EXPORT" data={exportData} headerColor="#CAE3C2" />
       </div>
+
+      <div className={styles.bigTable}>
+        <TableComponent title="NEED TO BE RESTOCKED" rowData={rowData1} lastColumnType="choose" backgroundColor="#FFFFE8B8" />  
+        <TableComponent 
+          title="STOCK" 
+          rowData={rowData2} 
+          lastColumnType="assign" 
+          backgroundColor="#ABD6D9BA"
+          toggleAssignContainer={toggleAssignContainer}
+        />
+      </div>
+
+      {showAssignContainer && (
+        <div className={styles.modal}>
+          <div onClick={toggleAssignContainer} className={styles.overlay}></div>
+            <div className={styles.assign_container}>
+              <div className={styles.assign_header}>ASSIGN</div>
+              <div className={`${styles.input_box} ${styles.driver_box}`}></div>
+              <div className="input-box time-box"></div>
+              <div className="arrow-icon time-arrow"></div>
+              <div className="input-box note-box"></div>
+              <div className="arrow-icon driver-arrow"></div>
+              <div className="label driver-label">Driver</div>
+              <div className="label time-label">Time</div>
+              <div className="label note-label">Note <br />(if needed)</div>
+              <div className="save-button"></div>
+              <div className="save-text" onClick={toggleAssignContainer}>Save</div>
+            </div>
+        </div>
+      )}
 
     </div>
   )
