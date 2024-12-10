f9227041ba0243257ece496e33a09c3b32fa8962
elfbe <huynhngocnhon2004@gmail.com>
2024-12-10 16:00:05 +0700

    Branch View

--- src/Pages/Branch_Product.jsx
+++ src/Pages/Branch_Product.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export const Branch_Product = () => {
+  const { branch_id } = useParams();
+
   return (
-    <div>Branch_Product</div>
+    <div>Branch_Product - {branch_id}</div>
   )
 }
